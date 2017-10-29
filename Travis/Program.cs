5162d6f1c8d6f81ab5705501ad77419140fa61ff
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-10-29 02:12:14 +0100

    Add new solution

---
+++ Travis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Travis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
