b17f5a4c3e50c00efa3f9c8977ebb573e3790e15
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-10-29 02:58:50 +0100

    Remove quotation mark to break build

--- Travis/Program.cs
+++ Travis/Program.cs
@@ -6,7 +6,7 @@ namespace Travis
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!);
         }
     }
 }
